b88c1c17b1ded16889988979c931d7d06eb3b3c6
Ayush-Petwal <ayushpetwal003@gmail.com>
2024-07-11 23:44:50 +0530

    Merge Sort

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+        <SortingVisualizer />
+    </div>
+  )
+}
+
+export default App
